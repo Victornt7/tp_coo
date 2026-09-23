5c087fa7f61d127030c743132bfd70a792fa77c9
Victor N <victor-neto@gmx.fr>
2026-09-23 11:37:21 +0200

    Defining Classes - 1

--- velos/high_level/models.py
+++ velos/high_level/models.py
@@ -2,56 +2,95 @@ from django.db import models
 
 
 class Pays(models.Model):
-    pass
+    nom = models.CharField(max_length=48)
+    tva = models.FloatField()
+    tarif_electrique = models.FloatField()
+    salaire_minimum = models.FloatField()
 
 
 class Ville(models.Model):
-    pass
+    nom = models.CharField(max_length=58)
+    taxe_immobiliere = models.FloatField()
+    prix_m2 = models.FloatField()
+    pays = models.ForeignKey(Pays, on_delete=models.PROTECT)
 
 
 class Machine(models.Model):
-    pass
+    nom = models.CharField(max_length=50)
+    prix = models.FloatField()
+    duree_de_vie = models.FloatField()
+    cout_maintenance = models.FloatField()
+    superficie = models.FloatField()
 
 
 class QuantiteMachine(models.Model):
-    pass
+    machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    nombre = models.IntegerField()
 
 
 class Lieu(models.Model):
-    pass
+    nom = models.CharField(max_length=50)
+    ville = models.ForeignKey(Ville, on_delete=models.PROTECT)
+    superficie = models.FloatField()
+    quantite_machines = models.ManyToManyField(Machine)
+    consommation_electrique = models.FloatField()
 
 
 class Transport(models.Model):
-    pass
+    nombre_palettes = models.IntegerField()
+    cout = models.FloatField()
+    delai = models.FloatField()
+    depart = models.ForeignKey(Lieu, on_delete=models.PROTECT)
+    arrivee = models.ForeignKey(Lieu, on_delete=models.PROTECT)
 
 
 class Operation(models.Model):
-    pass
+    nom = models.CharField(max_length=50)
+    operation_suivante = models.ForeignKey("self", on_delete=models.PROTECT)
+    cout = models.FloatField()
+    machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    quantite_produits = models.ForeignKey("QuantiteProduit", on_delete=models.PROTECT())
+    heures_de_travail = models.FloatField()
+    consomation_electrique = models.FloatField()
 
 
 class Produit(models.Model):
-    pass
+    nom = models.CharField(max_length=50)
+    prix_de_vente = models.FloatField()
+    duree_de_vie = models.FloatField()
+    nombre_par_palette = models.IntegerField()
+    operations = models.ForeignKey(Operation, on_delete=models.PROTECT)
 
 
 class PrixProduit(models.Model):
-    pass
+    produit = models.ForeignKey(Produit, on_delete=models.PROTECT)
+    prix_achat = models.FloatField()
 
 
 class Fournisseur(models.Model):
-    pass
+    nom = models.CharField(max_length=50)
+    prix_produit = models.ForeignKey(PrixProduit, on_delete=models.PROTECT)
 
 
 class QuantiteProduit(models.Model):
-    pass
+    produit = models.ForeignKey(Produit, on_delete=models.PROTECT)
+    nombre = models.IntegerField()
 
 
 class Stock(models.Model):
-    pass
+    quantite_produit = models.ForeignKey(QuantiteProduit, on_delete=models.PROTECT)
+    palettes_max = models.IntegerField()
 
 
 class PointDeVente(models.Model):
-    pass
+    nom = models.CharField(max_length=50)
+    lieu = models.ForeignKey(Lieu, on_delete=models.PROTECT)
+    heures_de_travail = models.FloatField()
+    stock = models.ForeignKey(Stock, on_delete=models.PROTECT)
 
 
 class Facture(models.Model):
-    pass
+    quantite_produit = models.ManyToManyField(QuantiteProduit)
+    reduction = models.FloatField()
+    point_de_vente = models.ForeignKey(PointDeVente, on_delete=models.PROTECT)
+    client = models.CharField(50)
