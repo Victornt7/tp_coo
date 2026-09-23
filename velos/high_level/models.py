d269a87c9aa5637ea2e57c75a11f4d620245624e
Victor N <victor-neto@gmx.fr>
2026-09-23 10:45:38 +0200

    Defining Classes - 1

--- velos/high_level/models.py
+++ velos/high_level/models.py
@@ -1,1 +1,57 @@
-# Create your models here.
+from django.db import models
+
+
+class Pays(models.Model):
+    pass
+
+
+class Ville(models.Model):
+    pass
+
+
+class Machine(models.Model):
+    pass
+
+
+class QuantiteMachine(models.Model):
+    pass
+
+
+class Lieu(models.Model):
+    pass
+
+
+class Transport(models.Model):
+    pass
+
+
+class Operation(models.Model):
+    pass
+
+
+class Produit(models.Model):
+    pass
+
+
+class PrixProduit(models.Model):
+    pass
+
+
+class Fournisseur(models.Model):
+    pass
+
+
+class QuantiteProduit(models.Model):
+    pass
+
+
+class Stock(models.Model):
+    pass
+
+
+class PointDeVente(models.Model):
+    pass
+
+
+class Facture(models.Model):
+    pass
