5cd783aa6112da8e5b4d7dc767620e70e0cad0b3
Victor N <victor-neto@gmx.fr>
2026-09-23 20:30:36 +0200

    Adding __str__(self) for each model

--- velos/high_level/models.py
+++ velos/high_level/models.py
@@ -7,12 +7,18 @@ class Pays(models.Model):
     tarif_electrique = models.FloatField()
     salaire_minimum = models.FloatField()
 
+    def __str__(self):
+        return self.nom
+
 
 class Ville(models.Model):
     nom = models.CharField(max_length=58)
     taxe_immobiliere = models.FloatField()
     prix_m2 = models.FloatField()
     pays = models.ForeignKey(Pays, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nom
 
 
 class Machine(models.Model):
@@ -22,10 +28,16 @@ class Machine(models.Model):
     cout_maintenance = models.FloatField()
     superficie = models.FloatField()
 
+    def __str__(self):
+        return self.nom
+
 
 class QuantiteMachine(models.Model):
     machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
     nombre = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.machine.nom} x {self.nombre}"
 
 
 class Lieu(models.Model):
@@ -35,6 +47,9 @@ class Lieu(models.Model):
     quantite_machines = models.ManyToManyField(Machine)
     consommation_electrique = models.FloatField()
 
+    def __str__(self):
+        return self.nom
+
 
 class Transport(models.Model):
     nombre_palettes = models.IntegerField()
@@ -42,6 +57,9 @@ class Transport(models.Model):
     delai = models.FloatField()
     depart = models.ForeignKey(Lieu, on_delete=models.PROTECT, related_name="depart")
     arrivee = models.ForeignKey(Lieu, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.depart.nom + " to " + self.arrivee.nom
 
 
 class Operation(models.Model):
@@ -53,6 +71,9 @@ class Operation(models.Model):
     heures_de_travail = models.FloatField()
     consomation_electrique = models.FloatField()
 
+    def __str__(self):
+        return self.nom
+
 
 class Produit(models.Model):
     nom = models.CharField(max_length=50)
@@ -61,25 +82,40 @@ class Produit(models.Model):
     nombre_par_palette = models.IntegerField()
     operations = models.ForeignKey(Operation, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.nom
+
 
 class PrixProduit(models.Model):
     produit = models.ForeignKey(Produit, on_delete=models.PROTECT)
     prix_achat = models.FloatField()
+
+    def __str__(self):
+        return f"{self.produit}: {self.prix_achat}"
 
 
 class Fournisseur(models.Model):
     nom = models.CharField(max_length=50)
     prix_produit = models.ForeignKey(PrixProduit, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.nom
+
 
 class QuantiteProduit(models.Model):
     produit = models.ForeignKey(Produit, on_delete=models.PROTECT)
     nombre = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.produit} x {self.nombre}"
+
 
 class Stock(models.Model):
     quantite_produit = models.ForeignKey(QuantiteProduit, on_delete=models.PROTECT)
     palettes_max = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.quantite_produit}, palettes max: {self.palettes_max}"
 
 
 class PointDeVente(models.Model):
@@ -88,9 +124,15 @@ class PointDeVente(models.Model):
     heures_de_travail = models.FloatField()
     stock = models.ForeignKey(Stock, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.nom
+
 
 class Facture(models.Model):
     quantite_produit = models.ManyToManyField(QuantiteProduit)
     reduction = models.FloatField()
     point_de_vente = models.ForeignKey(PointDeVente, on_delete=models.PROTECT)
     client = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.quantite_produit}, reduction: {self.reduction}, à {self.point_de_vente} pour {self.client}"
