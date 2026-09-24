f6905c7f3f1e05c362a5e491e9b303b551e9e08b
Victor N <victor-neto@gmx.fr>
2026-09-24 21:17:22 +0200

    Registering the models in admin.py

--- velos/high_level/admin.py
+++ velos/high_level/admin.py
@@ -1,1 +1,33 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import (
+    Facture,
+    Fournisseur,
+    Lieu,
+    Machine,
+    Operation,
+    Pays,
+    PointDeVente,
+    PrixProduit,
+    Produit,
+    QuantiteMachine,
+    QuantiteProduit,
+    Stock,
+    Transport,
+    Ville,
+)
+
+admin.site.register(Pays)
+admin.site.register(Ville)
+admin.site.register(Machine)
+admin.site.register(QuantiteMachine)
+admin.site.register(Lieu)
+admin.site.register(Transport)
+admin.site.register(Operation)
+admin.site.register(Produit)
+admin.site.register(PrixProduit)
+admin.site.register(Fournisseur)
+admin.site.register(QuantiteProduit)
+admin.site.register(Stock)
+admin.site.register(PointDeVente)
+admin.site.register(Facture)
